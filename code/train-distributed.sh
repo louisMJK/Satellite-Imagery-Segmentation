799c0e2957c7386acbe94264a5013bbc87988be6
Yunlei (Louis) Lu <luyunlei99@gmail.com>
2023-12-16 18:45:48 -0500

    finetune unet

--- code/train-distributed.sh
+++ code/train-distributed.sh
@@ -1,8 +1,23 @@
 torchrun --nproc_per_node=2 ./train.py \
-    --optim adam --lr-base 5e-4 --weight-decay 1e-5 \
+    --optim sgd --lr-base 1e-4 --momentum 0.9 --weight-decay 1e-4 \
     --sched cosine \
-    --epochs 100 \
-    
+    --epochs 40 \
+    --model-checkpoint ../checkpoints/model_jac_0.4604.pth
+
+
+torchrun --nproc_per_node=2 ./train.py \
+    --optim adam --lr-base 1e-4 --weight-decay 1e-4 \
+    --sched cosine \
+    --epochs 40 \
+    --model-checkpoint ../checkpoints/model_jac_0.4604.pth
+
+
+torchrun --nproc_per_node=2 ./train.py \
+    --optim sgd --lr-base 1e-4 --momentum 0.9 --weight-decay 0 \
+    --sched poly \
+    --epochs 40 \
+    --model-checkpoint ../checkpoints/model_jac_0.4604.pth
+
 
 # torchrun --nproc_per_node=2 ./train.py \
 #     --optim adam --lr-base 5e-4 --weight-decay 0 \
